018b1a8ba89f97b71ebf11106fe58c82f00724d2
lockje <joelislief@hotmail.com>
2014-01-09 22:00:24 +0100

    First commit to celebrate usage..

---
+++ Risk/src/nl/olie/games/Main.java
@@ -0,0 +1,16 @@
+package nl.olie.games;
+
+/**
+ * Created by Joel on 9-1-14.
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+        new Main();
+    }
+
+    public Main()
+    {
+        System.out.println("Hello world!");
+    }
+}
